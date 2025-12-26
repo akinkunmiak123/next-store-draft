60f44629b142904fc164809e763c7c7650ef674f
Akinkunmi Akinnitire <tetisimiak@gmail.com>
2025-12-26 20:59:40 +0100

    checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,39 +1,12 @@
-'use client'
-import axios from 'axios'
-import { useSearchParams } from 'next/navigation'
-import React, { useCallback } from 'react'
-import { loadStripe } from '@stripe/stripe-js'
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from '@stripe/react-stripe-js'
+import { Suspense } from 'react'
+import CheckoutClient from './CheckoutClient'
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-)
+export const dynamic = 'force-dynamic'
 
-
-function CheckoutPage() {
-    const searchParams = useSearchParams()
-
-    const orderId = searchParams.get('orderId')
-    const cartId = searchParams.get('cartId')
-
-    const fetchClientSecret = useCallback(async() => {
-        const response = await axios.post('/api/payment',{
-            orderId,cartId
-        })
-        return response.data.clientSecret;
-    },[])
-
-    const options = {fetchClientSecret}
-
+export default function CheckoutPage() {
   return (
-    <div id="checkout">
-      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-        <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
-    </div>
+    <Suspense fallback={<div>Loading checkout…</div>}>
+      <CheckoutClient />
+    </Suspense>
   )
 }
-export default CheckoutPage
