5c28f5505cfee993980ec3209303406947ddb1cd
Akinkunmi Akinnitire <tetisimiak@gmail.com>
2025-12-28 21:05:11 +0100

    pls

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,10 +8,11 @@ function AboutPage() {
         </span>
       </h1>
       <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
-        distinctio ducimus temporibus nobis autem laboriosam repellat, magni
-        fugiat minima excepturi neque, tenetur possimus nihil atque! Culpa nulla
-        labore nam?
+        We love building a store that puts people first. Our goal is to make
+        shopping simple, enjoyable, and reliable by offering carefully selected
+        products you can trust. We focus on quality, fair pricing, and a smooth
+        experience from search to checkout, so you can spend less time browsing
+        and more time enjoying what you buy.
       </p>
     </section>
   )
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -9,10 +9,19 @@ function Hero() {
         <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
           We are changing the way people shop
         </h1>
-        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum fugit ex alias sed, voluptate repellendus ad ullam repudiandae rerum quam.</p>
-        
-        <Button asChild size='lg' className="mt-10">
-          <Link href="/products"> Our Products </Link>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Discover a smarter, more convenient way to shop for quality products
+          you’ll love. We bring carefully selected items together in one place,
+          making it easy to find what you need without stress. From comfort and
+          style to value and reliability, every product is chosen to make your
+          everyday life better.
+        </p>
+
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products" className="text-white">
+            {' '}
+            Our Products{' '}
+          </Link>
         </Button>
       </div>
       <HeroCarousel />
