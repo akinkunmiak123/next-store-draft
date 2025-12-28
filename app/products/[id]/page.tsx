a3ee02df47b0cbef33a267ba655ae7367f085253
Akinkunmi Akinnitire <tetisimiak@gmail.com>
2025-12-28 20:25:00 +0100

    params

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -3,19 +3,25 @@ import { fetchSingleProduct, findExistingReview } from '@/utils/actions'
 import Image from 'next/image'
 import { formatCurrency } from '@/utils/format'
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton'
-import AddToCart from '@/components/single-product/AddToCart'
+import AddToCart from '@/components/single-product/AddToCart' 
 import ProductRating from '@/components/single-product/ProductRating'
 import ShareButton from '@/components/single-product/ShareButton'
 import SubmitReview from '@/components/reviews/SubmitReview'
 import ProductReviews from '@/components/reviews/ProductReviews'
 import { auth } from '@clerk/nextjs/server'
 
-async function SingleProductPage({ params }: any) {
-  const product = await fetchSingleProduct(params.id)
+
+type EditProductPageProps = {
+  params: Promise<{ id: string }>
+}
+async function SingleProductPage({ params }: EditProductPageProps) {
+   const { id } = await params
+  const product = await fetchSingleProduct(id)
   const { name, image, company, description, price } = product
   const dollarsAmount = formatCurrency(price)
-  const {userId} = await auth()
-  const reviewDoesNotExist = userId && !(await findExistingReview(userId,product.id))
+  const { userId } = await auth()
+  const reviewDoesNotExist =
+    userId && !(await findExistingReview(userId, product.id))
   // Pass productId to client component
   return (
     <section>
@@ -36,25 +42,24 @@ async function SingleProductPage({ params }: any) {
           <div className="flex gap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
             <div className="flex items-center gap-x-2">
-              <FavoriteToggleButton productId={params.id} />
-              <ShareButton name={name} productId={params.id} />
+              <FavoriteToggleButton productId={id} />
+              <ShareButton name={name} productId={id} />
             </div>
           </div>
 
-          <ProductRating productId={params.id} />
+          <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
           <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
             {dollarsAmount}
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-          <AddToCart productId={params.id} />
+          <AddToCart productId={id} />
         </div>
       </div>
 
       {/* Client component handles hook logic internally */}
-      <ProductReviews productId={params.id} />
-      {reviewDoesNotExist && <SubmitReview productId={params.id} />
-      }
+      <ProductReviews productId={id} />
+      {reviewDoesNotExist && <SubmitReview productId={id} />}
     </section>
   )
 }
--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,9 @@
-
 import {PrismaPg} from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
-
 const prismaClientSingleton = () => {
   return new PrismaClient({
     adapter: adapter
